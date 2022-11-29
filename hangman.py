7b514bfb36b8329098fe4268f10c4b17fa7956fb
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-11-29 20:25:21 +0100

    first hangman logic

--- hangman.py
+++ hangman.py
@@ -6,16 +6,25 @@ def hangman():
     word_list = ["carmageddon", "braveheart", "crystalball"]
     pick_word = random.choice(word_list)
 
-    empty_word = ""
+    empty_word = []
+    lives = 3
+
 
     for char in pick_word:
-        empty_word += "_"
+        empty_word.append("_")
 
-    letter = input("Guess a letter").lower()
+    while "_" in empty_word:
+        letter = input("Guess a letter:\n").lower()
+        for i in range(len(empty_word)):
+            if letter == pick_word[i]:
+                empty_word[i] = letter
 
-    for char in pick_word:
-        if char == letter:
-            print("right")
+        if not letter in empty_word:
+            lives -= 1
+        if lives <= 0:
+            print("YOU LOOSE")
+            break
+        print(empty_word)
+        print(lives)
 
-
-    print(empty_word)
+    print("finished")
