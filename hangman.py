e2b2b85b977e71673ed40eda24e02cf0f4992a21
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-11-29 16:32:37 +0100

    hangman first test

--- hangman.py
+++ hangman.py
@@ -1,3 +1,21 @@
+import random
+
 
 def hangman():
     print("hangman")
+    word_list = ["carmageddon", "braveheart", "crystalball"]
+    pick_word = random.choice(word_list)
+
+    empty_word = ""
+
+    for char in pick_word:
+        empty_word += "_"
+
+    letter = input("Guess a letter").lower()
+
+    for char in pick_word:
+        if char == letter:
+            print("right")
+
+
+    print(empty_word)
