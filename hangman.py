aece66bc07502459fd1f1ed73b173ebddbc07225
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-11-29 20:35:49 +0100

    hangman visuals

--- hangman.py
+++ hangman.py
@@ -3,28 +3,88 @@ import random
 
 def hangman():
     print("hangman")
+
+    stages = ['''
+      +---+
+      |   |
+      O   |
+     /|\  |
+     / \  |
+          |
+    =========
+    ''', '''
+      +---+
+      |   |
+      O   |
+     /|\  |
+     /    |
+          |
+    =========
+    ''', '''
+      +---+
+      |   |
+      O   |
+     /|\  |
+          |
+          |
+    =========
+    ''', '''
+      +---+
+      |   |
+      O   |
+     /|   |
+          |
+          |
+    =========''', '''
+      +---+
+      |   |
+      O   |
+      |   |
+          |
+          |
+    =========
+    ''', '''
+      +---+
+      |   |
+      O   |
+          |
+          |
+          |
+    =========
+    ''', '''
+      +---+
+      |   |
+          |
+          |
+          |
+          |
+    =========
+    ''']
+
     word_list = ["carmageddon", "braveheart", "crystalball"]
     pick_word = random.choice(word_list)
 
     empty_word = []
-    lives = 3
+    lives = 6
 
 
     for char in pick_word:
         empty_word.append("_")
 
     while "_" in empty_word:
-        letter = input("Guess a letter:\n").lower()
-        for i in range(len(empty_word)):
-            if letter == pick_word[i]:
-                empty_word[i] = letter
-
-        if not letter in empty_word:
-            lives -= 1
+        print(stages[lives])
         if lives <= 0:
             print("YOU LOOSE")
             break
         print(empty_word)
-        print(lives)
+        print("LIVES: " + str(lives))
+        letter = input("Guess a letter:\n").lower()
+        for i in range(len(empty_word)):
+            if letter == pick_word[i]:
+                empty_word[i] = letter
+        if not letter in empty_word:
+            lives -= 1
 
+    if not "_" in empty_word:
+        print("YOU WON")
     print("finished")
