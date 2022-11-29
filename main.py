daa9b01714492a45a92ed6b09a40ee6605aac96f
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-11-29 07:14:09 +0100

    hangman init

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from high_score import high_score
 from even_numbers import even_numbers
 from fizz_buzz import fizz_buzz
 from password_gen import password_gen
+from hangman import hangman
 
 # avg_height()
 # high_score()
 # even_numbers()
 # fizz_buzz()
-password_gen()
+# password_gen()
+hangman()
 
 
 
