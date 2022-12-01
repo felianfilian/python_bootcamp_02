19cd18bff1895aba823d134d0ee7d97075923fa9
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-01 18:33:44 +0100

    cipher system update

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,21 +1,14 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-def encrypt(plain_text, shift_amount):
+def crypt(plain_text, shift_amount, type):
     cipher_text = ""
+    shift_amount = shift_amount % len(alphabet)
+    if type == 1:
+        shift_amount *= -1
     for letter in plain_text:
         position = alphabet.index(letter)
         new_position = position + shift_amount
-        new_letter = alphabet[new_position]
-        cipher_text += new_letter
-    print(cipher_text)
-
-def decrypt(plain_text, shift_amount):
-    cipher_text = ""
-    for letter in plain_text:
-        position = alphabet.index(letter)
-        new_position = position - shift_amount
-        new_letter = alphabet[new_position]
-        cipher_text += new_letter
+        cipher_text += alphabet[new_position]
     print(cipher_text)
 
 def start():
@@ -23,8 +16,8 @@ def start():
     print("Encode")
     text = input("message:\n").lower()
     shift = int(input("shift number:\n"))
-    encrypt(text, shift)
+    crypt(text, shift, 0)
     print("Decode")
     text = input("message:\n").lower()
     shift = int(input("shift number:\n"))
-    decrypt(text, shift)
+    crypt(text, shift, 1)
