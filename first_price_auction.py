bc0c98d9cdc302707476224e7b9ca639432623fa
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-02 09:02:53 +0100

    auction version 1

--- first_price_auction.py
+++ first_price_auction.py
@@ -2,10 +2,17 @@
 
 def start():
     more_input = "y"
+    bids = {}
+    highest_bid = 0
+    winner = ""
     while more_input == "y":
         name = input("Your name:\n")
-        bid = input("Your bid:\n")
+        bid = int(input("Your bid:\n"))
+        bids[name] = bid
         more_input = input("More bidder? (y/n):\n")
-
-    highest_bid = 0
-    print("The highest bid: " + name + " with " + bid + " $")
+    for person in bids:
+        bid_amount = bids[person]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = person
+    print(f"the winner is {winner} with a bid of {highest_bid}")
